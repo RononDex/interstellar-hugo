e9dc8e9c0b6999bddda3c6878ec2a63ad38ebe46
RononDex <tinoheuberger@protonmail.com>
2022-01-25 15:03:17 +0100

    Make whole image post clickable in list view

--- assets/js/interstellar.js
+++ assets/js/interstellar.js
@@ -1,9 +1,11 @@
 $(function () {
-  if (functionsAfterDocumentReady !== undefined) {
+  if (window.functionsAfterDocumentReady && window.functionsAfterDocumentReady !== undefined) {
     for (var i = 0; i < functionsAfterDocumentReady.length; i++) {
       functionsAfterDocumentReady[i]();
     }
   }
+
+  MakeImagePostsClickable();
 });
 
 function addSvgOverlayToOpenSeadragonViewer(svgPath, viewer) {
@@ -44,4 +46,16 @@ function addSvgOverlayToOpenSeadragonViewer(svgPath, viewer) {
       //}
     }
   });
+}
+
+function MakeImagePostsClickable() {
+    $(".img-post-entry").each(function(t) {
+
+        $(this).attr("style", "cursor: pointer;");
+
+        $(this).click(function() { 
+            var postPermalink = $(this).find("a.permalink").attr("href");
+            window.location = postPermalink;
+        })
+    });
 }
