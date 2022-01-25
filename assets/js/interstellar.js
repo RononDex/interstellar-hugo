5385369fb421774048d9ab85990684f2a4d844b7
RononDex <tinoheuberger@protonmail.com>
2022-01-25 15:37:38 +0100

    Add a zoom percentage display to photos

--- assets/js/interstellar.js
+++ assets/js/interstellar.js
@@ -1,5 +1,8 @@
 $(function () {
-  if (window.functionsAfterDocumentReady && window.functionsAfterDocumentReady !== undefined) {
+  if (
+    window.functionsAfterDocumentReady &&
+    window.functionsAfterDocumentReady !== undefined
+  ) {
     for (var i = 0; i < functionsAfterDocumentReady.length; i++) {
       functionsAfterDocumentReady[i]();
     }
@@ -7,6 +10,25 @@ $(function () {
 
   MakeImagePostsClickable();
 });
+
+function addZoomDisplay(viewer, imageWidth) {
+  viewer.addHandler("resize", function() {
+      UpdatePercentageZoom(viewer)});
+  viewer.addHandler("animation", function(){
+      UpdatePercentageZoom(viewer)});
+  viewer.addHandler("open", function() {
+      UpdatePercentageZoom(viewer)});
+}
+
+function UpdatePercentageZoom(viewer) {
+  var tiledImage = viewer.world.getItemAt(0);
+  var originalZoom =
+    tiledImage.source.dimensions.x / viewer.viewport.getContainerSize().x;
+  var currentZoom = viewer.viewport.getZoom();
+
+  var zoomInPercent = (currentZoom / originalZoom) * 100;
+  $("#zoomPercentDisplay").html("Zoom: " + Math.round(zoomInPercent) + "%");
+}
 
 function addSvgOverlayToOpenSeadragonViewer(svgPath, viewer) {
   var overlay = viewer.svgOverlay();
@@ -49,13 +71,12 @@ function addSvgOverlayToOpenSeadragonViewer(svgPath, viewer) {
 }
 
 function MakeImagePostsClickable() {
-    $(".img-post-entry").each(function(t) {
+  $(".img-post-entry").each(function (t) {
+    $(this).attr("style", "cursor: pointer;");
 
-        $(this).attr("style", "cursor: pointer;");
-
-        $(this).click(function() { 
-            var postPermalink = $(this).find("a.permalink").attr("href");
-            window.location = postPermalink;
-        })
+    $(this).click(function () {
+      var postPermalink = $(this).find("a.permalink").attr("href");
+      window.location = postPermalink;
     });
+  });
 }
